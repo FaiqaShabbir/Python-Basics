7427e4a885cbc982785b3556f5819497a0e96866
FaiqaShabbir <90977625+FaiqaShabbir@users.noreply.github.com>
2022-05-05 22:19:58 +0500

    Faiqa Shabbir 05/05/2022

--- Operators/list_comprehension.py
+++ Operators/list_comprehension.py
@@ -9,5 +9,12 @@ mywish = [i * 2 for i in {1, 3, 5}]
 print(mywish)
 
 numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-sd = list(filter(lambda x: x % 3 == 0, numbers))
-print(sd)
+filt = list(filter(lambda x: x % 3 == 0, numbers))
+print(filt)
+ma = list(map(lambda x: x % 3 == 0, numbers))
+print(ma)
+
+myset = {1, 2, 3}
+makelist = lambda w: list(w)
+mylist1 = makelist(myset)
+print(mylist1)
